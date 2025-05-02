5da39376c6efc741bd8902f0aba1af98e7d1264d
yoo gayeong <asom8650@gmail.com>
2025-05-02 14:53:35 +0900

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { Login } from "./login/entities/login.entity";
       password: "1234",
       database: "projects", // DB 이름
       synchronize: true, // 개발 환경에서만 true (자동 테이블 생성)
-      entities: [User, Project,Login], // 전체 엔티티 등록
+      entities: [User, Project, Login], // 전체 엔티티 등록
     }),
     UsersModule,
     ProjectsModule,
@@ -29,4 +29,4 @@ import { Login } from "./login/entities/login.entity";
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
